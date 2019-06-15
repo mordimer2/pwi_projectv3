f701355d22929038480d99d4e65921b5d0feadaf
mordimer2 <mjanuszewski57@gmail.com>
2019-06-15 12:39:04 +0200

    UpdateDB v12

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), # static directory (in the top level directory) for local testing
 ]
 
+django_heroku.settings(locals())
