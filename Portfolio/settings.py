4414b173fb96002e814e7b973ca5f141fcc1a04a
mordimer2 <mjanuszewski57@gmail.com>
2019-06-15 12:08:22 +0200

    init v10

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'aa')
+SECRET_KEY = os.getenv('SECRET_KEY', 'vawuzb)_js)czgqkt$!g!2afic%kvo!jyoa49!hmzx)&_*c0dp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False')=="True"
+DEBUG = os.getenv('DEBUG', 'True')=="True"
 
 # Application definition
 
